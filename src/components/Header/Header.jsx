54b314135afd8dcd77d5e01012525518b032348c
Leoni Araujo <leoni.araujo@compasso.com.br>
2020-09-08 14:34:42 -0300

    criado header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+
+function Header() {
+    return (
+        <div className='header-bg'>
+            <h1 className='header-logo'>CountdownTimer</h1>
+        </div>
+    )
+}
+
+export default Header;
